2414bd4afc4d3774552851dd1f7458116f824f94
Dcrew <dean.reynolds@live.co.uk>
2020-03-12 04:28:42 +0000

    add source

---
+++ src/BitDataIO/BitDataReader.cs
@@ -0,0 +1,387 @@
+﻿using System;
+using System.Text;
+
+namespace BitDataIO
+{
+    public class BitDataReader
+    {
+        public static byte ReadByte(byte[] fromBuffer, int numberOfBits, int readBitOffset)
+        {
+            int bytePtr = readBitOffset >> 3;
+            int startReadAtIndex = readBitOffset - (bytePtr * 8);
+            if (startReadAtIndex == 0 && numberOfBits == 8)
+                return fromBuffer[bytePtr];
+            byte returnValue = (byte)(fromBuffer[bytePtr] >> startReadAtIndex);
+            int numberOfBitsInSecondByte = numberOfBits - (8 - startReadAtIndex);
+            if (numberOfBitsInSecondByte < 1)
+                return (byte)(returnValue & (255 >> (8 - numberOfBits)));
+            byte second = fromBuffer[bytePtr + 1];
+            second &= (byte)(255 >> (8 - numberOfBitsInSecondByte));
+            return (byte)(returnValue | (byte)(second << (numberOfBits - numberOfBitsInSecondByte)));
+        }
+        public static void ReadBytes(byte[] fromBuffer, int numberOfBytes, int readBitOffset, byte[] destination, int destinationByteOffset)
+        {
+            int readPtr = readBitOffset >> 3;
+            int startReadAtIndex = readBitOffset - (readPtr * 8);
+            if (startReadAtIndex == 0)
+            {
+                Buffer.BlockCopy(fromBuffer, readPtr, destination, destinationByteOffset, numberOfBytes);
+                return;
+            }
+            int secondPartLen = 8 - startReadAtIndex;
+            int secondMask = 255 >> secondPartLen;
+            for (int i = 0; i < numberOfBytes; i++)
+            {
+                int b = fromBuffer[readPtr] >> startReadAtIndex;
+                readPtr++;
+                int second = fromBuffer[readPtr] & secondMask;
+                destination[destinationByteOffset++] = (byte)(b | (second << secondPartLen));
+            }
+        }
+        public static ushort ReadUInt16(byte[] fromBuffer, int numberOfBits, int readBitOffset)
+        {
+            ushort returnValue;
+            if (numberOfBits <= 8)
+            {
+                returnValue = ReadByte(fromBuffer, numberOfBits, readBitOffset);
+                return returnValue;
+            }
+            returnValue = ReadByte(fromBuffer, 8, readBitOffset);
+            numberOfBits -= 8;
+            readBitOffset += 8;
+            if (numberOfBits <= 8)
+                returnValue |= (ushort)(ReadByte(fromBuffer, numberOfBits, readBitOffset) << 8);
+#if BIGENDIAN
+			uint retVal = returnValue;
+			retVal = ((retVal & 0x0000ff00) >> 8) | ((retVal & 0x000000ff) << 8);
+			return (ushort)retVal;
+#else
+            return returnValue;
+#endif
+        }
+        public static uint ReadUInt32(byte[] fromBuffer, int numberOfBits, int readBitOffset)
+        {
+            uint returnValue;
+            if (numberOfBits <= 8)
+            {
+                returnValue = ReadByte(fromBuffer, numberOfBits, readBitOffset);
+                return returnValue;
+            }
+            returnValue = ReadByte(fromBuffer, 8, readBitOffset);
+            numberOfBits -= 8;
+            readBitOffset += 8;
+            if (numberOfBits <= 8)
+            {
+                returnValue |= (uint)(ReadByte(fromBuffer, numberOfBits, readBitOffset) << 8);
+                return returnValue;
+            }
+            returnValue |= (uint)(ReadByte(fromBuffer, 8, readBitOffset) << 8);
+            numberOfBits -= 8;
+            readBitOffset += 8;
+            if (numberOfBits <= 8)
+            {
+                uint r = ReadByte(fromBuffer, numberOfBits, readBitOffset);
+                r <<= 16;
+                returnValue |= r;
+                return returnValue;
+            }
+            returnValue |= (uint)(ReadByte(fromBuffer, 8, readBitOffset) << 16);
+            numberOfBits -= 8;
+            readBitOffset += 8;
+            returnValue |= (uint)(ReadByte(fromBuffer, numberOfBits, readBitOffset) << 24);
+#if BIGENDIAN
+			return ((returnValue & 0xff000000) >> 24) | ((returnValue & 0x00ff0000) >> 8) | ((returnValue & 0x0000ff00) << 8) | ((returnValue & 0x000000ff) << 24);
+#else
+            return returnValue;
+#endif
+        }
+
+        public static int BitsToHoldUInt(uint value)
+        {
+            var bits = 1;
+            while ((value >>= 1) != 0)
+                bits++;
+            return bits;
+        }
+        public static int BitsToHoldULong(ulong value)
+        {
+            var bits = 1;
+            while ((value >>= 1) != 0)
+                bits++;
+            return bits;
+        }
+
+        public byte[] Data => _data;
+
+        public int LengthBits => _lengthBits;
+        public int LengthBytes => (_lengthBits + 7) >> 3;
+        public int ReadBits => _readBits;
+        public bool EndOfData => ReadBits >= LengthBits;
+
+        protected byte[] _data;
+        protected int _lengthBits;
+        protected int _readBits;
+
+        public bool ReadBool()
+        {
+            var retval = ReadByte(_data, 1, _readBits);
+            _readBits += 1;
+            return (retval == 1);
+        }
+
+        public sbyte ReadSByte()
+        {
+            var retval = ReadByte(_data, 8, _readBits);
+            _readBits += 8;
+            return (sbyte)retval;
+        }
+        public byte ReadByte()
+        {
+            var retval = ReadByte(_data, 8, _readBits);
+            _readBits += 8;
+            return retval;
+        }
+        public bool ReadByte(out byte result)
+        {
+            if (_lengthBits - _readBits < 8)
+            {
+                result = 0;
+                return false;
+            }
+            result = ReadByte(_data, 8, _readBits);
+            _readBits += 8;
+            return true;
+        }
+        public byte ReadByte(int numberOfBits)
+        {
+            var retval = ReadByte(_data, numberOfBits, _readBits);
+            _readBits += numberOfBits;
+            return retval;
+        }
+        public byte[] ReadBytes(int numberOfBytes)
+        {
+            var retval = new byte[numberOfBytes];
+            ReadBytes(_data, numberOfBytes, _readBits, retval, 0);
+            _readBits += (8 * numberOfBytes);
+            return retval;
+        }
+        public bool ReadBytes(int numberOfBytes, out byte[] result)
+        {
+            if (_lengthBits - _readBits + 7 < (numberOfBytes * 8))
+            {
+                result = null;
+                return false;
+            }
+            result = new byte[numberOfBytes];
+            ReadBytes(_data, numberOfBytes, _readBits, result, 0);
+            _readBits += (8 * numberOfBytes);
+            return true;
+        }
+        public void ReadBytes(byte[] into, int offset, int numberOfBytes)
+        {
+            ReadBytes(_data, numberOfBytes, _readBits, into, offset);
+            _readBits += (8 * numberOfBytes);
+            return;
+        }
+
+        public short ReadShort()
+        {
+            var retval = ReadUInt16(_data, 16, _readBits);
+            _readBits += 16;
+            return (short)retval;
+        }
+        public ushort ReadUShort()
+        {
+            var retval = ReadUInt16(_data, 16, _readBits);
+            _readBits += 16;
+            return (ushort)retval;
+        }
+
+        public int ReadInt()
+        {
+            var retval = ReadUInt32(_data, 32, _readBits);
+            _readBits += 32;
+            return (int)retval;
+        }
+        public bool ReadInt(out int result)
+        {
+            if (_lengthBits - _readBits < 32)
+            {
+                result = 0;
+                return false;
+            }
+            result = (int)ReadUInt32(_data, 32, _readBits);
+            _readBits += 32;
+            return true;
+        }
+        public int ReadInt(int numberOfBits)
+        {
+            var retval = ReadUInt32(_data, numberOfBits, _readBits);
+            _readBits += numberOfBits;
+            if (numberOfBits == 32)
+                return (int)retval;
+            var signBit = 1 << (numberOfBits - 1);
+            if ((retval & signBit) == 0)
+                return (int)retval;
+            unchecked
+            {
+                var mask = ((uint)-1) >> (33 - numberOfBits);
+                var tmp = (retval & mask) + 1;
+                return -((int)tmp);
+            }
+        }
+        public uint ReadUInt()
+        {
+            var retval = ReadUInt32(_data, 32, _readBits);
+            _readBits += 32;
+            return retval;
+        }
+        public bool ReadUInt(out uint result)
+        {
+            if (_lengthBits - _readBits < 32)
+            {
+                result = 0;
+                return false;
+            }
+            result = ReadUInt32(_data, 32, _readBits);
+            _readBits += 32;
+            return true;
+        }
+        public uint ReadUInt(int numberOfBits)
+        {
+            var retval = ReadUInt32(_data, numberOfBits, _readBits);
+            _readBits += numberOfBits;
+            return retval;
+        }
+        public int ReadRangedInt(int min, int max)
+        {
+            var range = (uint)(max - min);
+            var numBits = BitsToHoldUInt(range);
+            var rvalue = ReadUInt(numBits);
+            return (int)(min + rvalue);
+        }
+        public uint ReadVariableUInt()
+        {
+            int num1 = 0,
+                num2 = 0;
+            while (_lengthBits - _readBits >= 8)
+            {
+                var num3 = ReadByte();
+                num1 |= (num3 & 0x7f) << num2;
+                num2 += 7;
+                if ((num3 & 0x80) == 0)
+                    return (uint)num1;
+            }
+            return (uint)num1;
+        }
+
+        public float ReadFloat()
+        {
+            if ((_readBits & 7) == 0)
+            {
+                var retval = BitConverter.ToSingle(_data, _readBits >> 3);
+                _readBits += 32;
+                return retval;
+            }
+            var bytes = new byte[4];
+            ReadBytes(bytes, 0, 4);
+            return BitConverter.ToSingle(bytes, 0);
+        }
+        public bool ReadFloat(out float result)
+        {
+            if (_lengthBits - _readBits < 32)
+            {
+                result = 0;
+                return false;
+            }
+            if ((_readBits & 7) == 0)
+            {
+                result = BitConverter.ToSingle(_data, _readBits >> 3);
+                _readBits += 32;
+                return true;
+            }
+            var bytes = new byte[4];
+            ReadBytes(bytes, 0, 4);
+            result = BitConverter.ToSingle(bytes, 0);
+            return true;
+        }
+        public float ReadRangedFloat(float min, float max, int numberOfBits)
+        {
+            var range = max - min;
+            var maxVal = (1 << numberOfBits) - 1;
+            var encodedVal = (float)ReadUInt(numberOfBits);
+            var unit = encodedVal / maxVal;
+            return min + (unit * range);
+        }
+
+        public long ReadLong()
+        {
+            unchecked
+            {
+                var retval = ReadULong();
+                var longRetval = (long)retval;
+                return longRetval;
+            }
+        }
+        public long ReadLong(int numberOfBits) => (long)ReadULong(numberOfBits);
+        public ulong ReadULong()
+        {
+            var low = ReadUInt32(_data, 32, _readBits);
+            _readBits += 32;
+            var high = ReadUInt32(_data, 32, _readBits);
+            var retval = low + (high << 32);
+            _readBits += 32;
+            return retval;
+        }
+        public ulong ReadULong(int numberOfBits)
+        {
+            ulong retval;
+            if (numberOfBits <= 32)
+                retval = ReadUInt32(_data, numberOfBits, _readBits);
+            else
+            {
+                retval = ReadUInt32(_data, 32, _readBits);
+                retval |= (ulong)ReadUInt32(_data, numberOfBits - 32, _readBits + 32) << 32;
+            }
+            _readBits += numberOfBits;
+            return retval;
+        }
+        public long ReadRangedLong(long min, long max)
+        {
+            var range = (ulong)(max - min);
+            var numBits = BitsToHoldULong(range);
+            var rvalue = ReadULong(numBits);
+            return min + (long)rvalue;
+        }
+
+        public double ReadDouble()
+        {
+            if ((_readBits & 7) == 0)
+            {
+                var retval = BitConverter.ToDouble(_data, _readBits >> 3);
+                _readBits += 64;
+                return retval;
+            }
+            return BitConverter.ToDouble(ReadBytes(8), 0);
+        }
+
+        public string ReadString()
+        {
+            var byteLen = (int)ReadVariableUInt();
+            if (byteLen <= 0)
+                return string.Empty;
+            if ((ulong)(_lengthBits - _readBits) < ((ulong)byteLen * 8))
+            {
+                _readBits = _lengthBits;
+                return null;
+            }
+            if ((_readBits & 7) == 0)
+            {
+                var retval = Encoding.UTF8.GetString(_data, _readBits >> 3, byteLen);
+                _readBits += 8 * byteLen;
+                return retval;
+            }
+            return Encoding.UTF8.GetString(ReadBytes(byteLen), 0, byteLen);
+        }
+    }
+}
---
+++ src/BitDataIO/BitDataWriter.cs
@@ -0,0 +1,368 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace BitDataIO
+{
+    public class BitDataWriter
+    {
+        [StructLayout(LayoutKind.Explicit)]
+        struct SingleUIntUnion
+        {
+            [FieldOffset(0)]
+            public float SingleValue;
+            [FieldOffset(0)]
+            public uint UIntValue;
+        }
+
+        const int _overAllocateAmount = 4;
+
+        public static void WriteByte(byte source, int numberOfBits, byte[] destination, int destBitOffset)
+        {
+            source = (byte)(source & (0xFF >> (8 - numberOfBits)));
+            int p = destBitOffset >> 3;
+            int bitsUsed = destBitOffset & 0x7;
+            int bitsFree = 8 - bitsUsed;
+            int bitsLeft = bitsFree - numberOfBits;
+            if (bitsLeft >= 0)
+            {
+                int mask = (0xFF >> bitsFree) | (0xFF << (8 - bitsLeft));
+                destination[p] = (byte)((destination[p] & mask) | (source << bitsUsed));
+                return;
+            }
+            destination[p] = (byte)((destination[p] & (0xFF >> bitsFree)) | (source << bitsUsed));
+            p += 1;
+            destination[p] = (byte)((destination[p] & (0xFF << (numberOfBits - bitsFree))) | (source >> bitsFree));
+        }
+        public static void WriteBytes(byte[] source, int sourceByteOffset, int numberOfBytes, byte[] destination, int destBitOffset)
+        {
+            int dstBytePtr = destBitOffset >> 3;
+            int firstPartLen = (destBitOffset % 8);
+            if (firstPartLen == 0)
+            {
+                Buffer.BlockCopy(source, sourceByteOffset, destination, dstBytePtr, numberOfBytes);
+                return;
+            }
+            int lastPartLen = 8 - firstPartLen;
+            for (int i = 0; i < numberOfBytes; i++)
+            {
+                byte src = source[sourceByteOffset + i];
+                destination[dstBytePtr] &= (byte)(255 >> lastPartLen);
+                destination[dstBytePtr] |= (byte)(src << firstPartLen);
+                dstBytePtr++;
+                destination[dstBytePtr] &= (byte)(255 << firstPartLen);
+                destination[dstBytePtr] |= (byte)(src >> lastPartLen);
+            }
+        }
+        public static void WriteUInt16(ushort source, int numberOfBits, byte[] destination, int destinationBitOffset)
+        {
+            if (numberOfBits == 0)
+                return;
+#if BIGENDIAN
+			uint intSource = source;
+			intSource = ((intSource & 0x0000ff00) >> 8) | ((intSource & 0x000000ff) << 8);
+			source = (ushort)intSource;
+#endif
+            if (numberOfBits <= 8)
+            {
+                WriteByte((byte)source, numberOfBits, destination, destinationBitOffset);
+                return;
+            }
+            WriteByte((byte)source, 8, destination, destinationBitOffset);
+            numberOfBits -= 8;
+            if (numberOfBits > 0)
+                WriteByte((byte)(source >> 8), numberOfBits, destination, destinationBitOffset + 8);
+        }
+        public static int WriteUInt32(uint source, int numberOfBits, byte[] destination, int destinationBitOffset)
+        {
+#if BIGENDIAN
+			source = ((source & 0xff000000) >> 24) | ((source & 0x00ff0000) >> 8) | ((source & 0x0000ff00) << 8) | ((source & 0x000000ff) << 24);
+#endif
+            int returnValue = destinationBitOffset + numberOfBits;
+            if (numberOfBits <= 8)
+            {
+                WriteByte((byte)source, numberOfBits, destination, destinationBitOffset);
+                return returnValue;
+            }
+            WriteByte((byte)source, 8, destination, destinationBitOffset);
+            destinationBitOffset += 8;
+            numberOfBits -= 8;
+            if (numberOfBits <= 8)
+            {
+                WriteByte((byte)(source >> 8), numberOfBits, destination, destinationBitOffset);
+                return returnValue;
+            }
+            WriteByte((byte)(source >> 8), 8, destination, destinationBitOffset);
+            destinationBitOffset += 8;
+            numberOfBits -= 8;
+            if (numberOfBits <= 8)
+            {
+                WriteByte((byte)(source >> 16), numberOfBits, destination, destinationBitOffset);
+                return returnValue;
+            }
+            WriteByte((byte)(source >> 16), 8, destination, destinationBitOffset);
+            destinationBitOffset += 8;
+            numberOfBits -= 8;
+            WriteByte((byte)(source >> 24), numberOfBits, destination, destinationBitOffset);
+            return returnValue;
+        }
+        public static int WriteUInt64(ulong source, int numberOfBits, byte[] destination, int destinationBitOffset)
+        {
+#if BIGENDIAN
+			source = ((source & 0xff00000000000000L) >> 56) |
+				((source & 0x00ff000000000000L) >> 40) |
+				((source & 0x0000ff0000000000L) >> 24) |
+				((source & 0x000000ff00000000L) >> 8) |
+				((source & 0x00000000ff000000L) << 8) |
+				((source & 0x0000000000ff0000L) << 24) |
+				((source & 0x000000000000ff00L) << 40) |
+				((source & 0x00000000000000ffL) << 56);
+#endif
+            int returnValue = destinationBitOffset + numberOfBits;
+            if (numberOfBits <= 8)
+            {
+                WriteByte((byte)source, numberOfBits, destination, destinationBitOffset);
+                return returnValue;
+            }
+            WriteByte((byte)source, 8, destination, destinationBitOffset);
+            destinationBitOffset += 8;
+            numberOfBits -= 8;
+            if (numberOfBits <= 8)
+            {
+                WriteByte((byte)(source >> 8), numberOfBits, destination, destinationBitOffset);
+                return returnValue;
+            }
+            WriteByte((byte)(source >> 8), 8, destination, destinationBitOffset);
+            destinationBitOffset += 8;
+            numberOfBits -= 8;
+            if (numberOfBits <= 8)
+            {
+                WriteByte((byte)(source >> 16), numberOfBits, destination, destinationBitOffset);
+                return returnValue;
+            }
+            WriteByte((byte)(source >> 16), 8, destination, destinationBitOffset);
+            destinationBitOffset += 8;
+            numberOfBits -= 8;
+            if (numberOfBits <= 8)
+            {
+                WriteByte((byte)(source >> 24), numberOfBits, destination, destinationBitOffset);
+                return returnValue;
+            }
+            WriteByte((byte)(source >> 24), 8, destination, destinationBitOffset);
+            destinationBitOffset += 8;
+            numberOfBits -= 8;
+            if (numberOfBits <= 8)
+            {
+                WriteByte((byte)(source >> 32), numberOfBits, destination, destinationBitOffset);
+                return returnValue;
+            }
+            WriteByte((byte)(source >> 32), 8, destination, destinationBitOffset);
+            destinationBitOffset += 8;
+            numberOfBits -= 8;
+            if (numberOfBits <= 8)
+            {
+                WriteByte((byte)(source >> 40), numberOfBits, destination, destinationBitOffset);
+                return returnValue;
+            }
+            WriteByte((byte)(source >> 40), 8, destination, destinationBitOffset);
+            destinationBitOffset += 8;
+            numberOfBits -= 8;
+            if (numberOfBits <= 8)
+            {
+                WriteByte((byte)(source >> 48), numberOfBits, destination, destinationBitOffset);
+                return returnValue;
+            }
+            WriteByte((byte)(source >> 48), 8, destination, destinationBitOffset);
+            destinationBitOffset += 8;
+            numberOfBits -= 8;
+            if (numberOfBits <= 8)
+            {
+                WriteByte((byte)(source >> 56), numberOfBits, destination, destinationBitOffset);
+                return returnValue;
+            }
+            WriteByte((byte)(source >> 56), 8, destination, destinationBitOffset);
+            destinationBitOffset += 8;
+            numberOfBits -= 8;
+            return returnValue;
+        }
+
+        public static int BitsToHoldUInt(uint value)
+        {
+            var bits = 1;
+            while ((value >>= 1) != 0)
+                bits++;
+            return bits;
+        }
+        public static int BitsToHoldULong(ulong value)
+        {
+            var bits = 1;
+            while ((value >>= 1) != 0)
+                bits++;
+            return bits;
+        }
+
+        static uint SwapByteOrder(uint value) => ((value & 0xff000000) >> 24) | ((value & 0x00ff0000) >> 8) | ((value & 0x0000ff00) << 8) | ((value & 0x000000ff) << 24);
+        static ulong SwapByteOrder(ulong value) => ((value & 0xff00000000000000L) >> 56) | ((value & 0x00ff000000000000L) >> 40) | ((value & 0x0000ff0000000000L) >> 24) | ((value & 0x000000ff00000000L) >> 8) | ((value & 0x00000000ff000000L) << 8) | ((value & 0x0000000000ff0000L) << 24) | ((value & 0x000000000000ff00L) << 40) | ((value & 0x00000000000000ffL) << 56);
+
+        public byte[] Data => _data;
+
+        public int LengthBits => _lengthBits;
+        public int LengthBytes => (_lengthBits + 7) >> 3;
+
+        protected byte[] _data;
+        protected int _lengthBits;
+
+        public void Write(bool value)
+        {
+            EnsureSize(_lengthBits + 1);
+            WriteByte(value ? (byte)1 : (byte)0, 1, _data, _lengthBits);
+            _lengthBits += 1;
+        }
+
+        public void Write(sbyte source)
+        {
+            EnsureSize(_lengthBits + 8);
+            WriteByte((byte)source, 8, _data, _lengthBits);
+            _lengthBits += 8;
+        }
+        public void Write(byte source)
+        {
+            EnsureSize(_lengthBits + 8);
+            WriteByte(source, 8, _data, _lengthBits);
+            _lengthBits += 8;
+        }
+        public void Write(byte[] source)
+        {
+            var bits = source.Length * 8;
+            EnsureSize(_lengthBits + bits);
+            WriteBytes(source, 0, source.Length, _data, _lengthBits);
+            _lengthBits += bits;
+        }
+
+        public void Write(short source)
+        {
+            EnsureSize(_lengthBits + 16);
+            WriteUInt16((ushort)source, 16, _data, _lengthBits);
+            _lengthBits += 16;
+        }
+        public void Write(ushort source)
+        {
+            EnsureSize(_lengthBits + 16);
+            WriteUInt16(source, 16, _data, _lengthBits);
+            _lengthBits += 16;
+        }
+
+        public void Write(int source)
+        {
+            EnsureSize(_lengthBits + 32);
+            WriteUInt32((uint)source, 32, _data, _lengthBits);
+            _lengthBits += 32;
+        }
+        public void Write(uint source)
+        {
+            EnsureSize(_lengthBits + 32);
+            WriteUInt32(source, 32, _data, _lengthBits);
+            _lengthBits += 32;
+        }
+        public void Write(uint source, int numberOfBits)
+        {
+            EnsureSize(_lengthBits + numberOfBits);
+            WriteUInt32(source, numberOfBits, _data, _lengthBits);
+            _lengthBits += numberOfBits;
+        }
+        public int WriteRangedInt(int min, int max, int value)
+        {
+            var range = (uint)(max - min);
+            var numBits = BitsToHoldUInt(range);
+            var rvalue = (uint)(value - min);
+            Write(rvalue, numBits);
+            return numBits;
+        }
+        public int WriteVariableUInt(uint value)
+        {
+            var retval = 1;
+            var num1 = value;
+            while (num1 >= 0x80)
+            {
+                Write((byte)(num1 | 0x80));
+                num1 = num1 >> 7;
+                retval++;
+            }
+            Write((byte)num1);
+            return retval;
+        }
+
+        public void Write(float source)
+        {
+            SingleUIntUnion su;
+            su.UIntValue = 0;
+            su.SingleValue = source;
+#if BIGENDIAN
+			su.UIntValue = NetUtility.SwapByteOrder(su.UIntValue);
+#endif
+            Write(su.UIntValue);
+        }
+        public void WriteRangedFloat(float value, float min, float max, int numberOfBits)
+        {
+            var range = max - min;
+            var unit = (value - min) / range;
+            var maxVal = (1 << numberOfBits) - 1;
+            Write((uint)(maxVal * unit), numberOfBits);
+        }
+
+        public void Write(long source)
+        {
+            EnsureSize(_lengthBits + 64);
+            var usource = (ulong)source;
+            WriteUInt64(usource, 64, _data, _lengthBits);
+            _lengthBits += 64;
+        }
+        public void Write(ulong source)
+        {
+            EnsureSize(_lengthBits + 64);
+            WriteUInt64(source, 64, _data, _lengthBits);
+            _lengthBits += 64;
+        }
+        public void Write(ulong source, int numberOfBits)
+        {
+            EnsureSize(_lengthBits + numberOfBits);
+            WriteUInt64(source, numberOfBits, _data, _lengthBits);
+            _lengthBits += numberOfBits;
+        }
+        public int WriteRangedLong(long min, long max, long value)
+        {
+            var range = (ulong)(max - min);
+            var numBits = BitsToHoldULong(range);
+            var rvalue = (ulong)(value - min);
+            Write(rvalue, numBits);
+            return numBits;
+        }
+
+        public void Write(double source)
+        {
+            var val = BitConverter.GetBytes(source);
+            Write(val);
+        }
+
+        public void Write(string source)
+        {
+            if (string.IsNullOrEmpty(source))
+            {
+                WriteVariableUInt(0);
+                return;
+            }
+            var bytes = Encoding.UTF8.GetBytes(source);
+            WriteVariableUInt((uint)bytes.Length);
+            Write(bytes);
+        }
+
+        protected void EnsureSize(int bits)
+        {
+            var byteLen = ((bits + 7) >> 3);
+            if (_data == null)
+                _data = new byte[byteLen + _overAllocateAmount];
+            else if (_data.Length < byteLen)
+                Array.Resize(ref _data, byteLen + _overAllocateAmount);
+        }
+    }
+}
